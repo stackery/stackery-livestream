73edfd299bc46f773c511c37b6a45a9ac90c17a2
Sam Goldstein <sam@stackery.io>
2019-05-15 10:33:14 -0700

    Added producer code

--- src/Producer/index.js
+++ src/Producer/index.js
@@ -1,6 +1,32 @@
+const aws = require('aws-sdk');
+const sqs = new aws.SQS();
+const sns = new aws.SNS();
+const kinesis = new aws.Kinesis();
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  const message = JSON.stringify(event);
+
+  const sqsResponse = await sqs.sendMessage({
+    QueueUrl: process.env.QUEUE_URL,
+    MessageBody: message
+  }).promise();
+
+  console.log(sqsResponse);
+
+  const snsResponse = await sns.publish({
+    Message: message,
+    TopicArn: process.env.TOPIC_ARN
+  }).promise();
+  console.log(snsResponse);
+
+  const kinesisResponse = await kinesis.putRecord({
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: '1',
+    Data: message
+
+  }).promise();
+  console.log(kinesisResponse);
 
   return {};
 };
